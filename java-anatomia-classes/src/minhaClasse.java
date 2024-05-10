c163c64a59276b18bff5ae8ffeb5bdf6f5fb9428
Graeff01 <douglasggraeff@icloud.com>
2024-05-10 15:20:10 -0300

    alterações da segunda parte do curso

--- java-anatomia-classes/src/minhaClasse.java
+++ java-anatomia-classes/src/minhaClasse.java
@@ -2,7 +2,12 @@ public class minhaClasse {
 
     public static void main (String [] args) {
 
-        System.out.println("Olá, Mundo!");
-} 
+        System.out.println("Olá, visitante!");
+        System.out.println("Escrevendo o código acima, vc consegue se expressar impondo informações entre as aspas!");
 
+        double PI = 3.14159;
+        int Estados_brasileiros = 26;
+        int ANO_2024 = 2024;   
+
+} 
 }
